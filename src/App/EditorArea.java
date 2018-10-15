353439f7ccb9adae292a42f94e745a78d5b81848
Alexander Komshanov <a.kosmhanovjr@gmail.com>
2018-10-15 17:41:20 +0300

    added testapp

--- src/App/EditorArea.java
+++ src/App/EditorArea.java
@@ -3,33 +3,39 @@ package App;
 import javax.swing.*;
 import javax.swing.text.*;
 import javax.swing.text.html.HTMLDocument;
-import javax.swing.text.html.HTMLWriter;
-import javax.swing.text.rtf.RTFEditorKit;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.HTMLWriter;;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class EditorArea extends JTextPane implements KeyListener{
+public class EditorArea extends JTextPane implements KeyListener {
     private String fileName;
     private final String PATH = "files/";
     private final String extension = ".html";
     private JLabel label;
 
 
-    public EditorArea(JLabel label){
+    public EditorArea(JLabel label) {
         this.label = label;
-        this.fileName = "default";
-        this.label.setText(fileName+extension);
+        setFileName(" ");
         this.addKeyListener(this);
         this.setContentType("text/html");
     }
 
-    String getFileName(){
+    public String getFileName() {
         return fileName + extension;
+    }
+
+    private void setFileName(String name) {
+        this.fileName = name;
+        if (name.trim().isEmpty()) {
+            label.setText(name);
+        } else {
+            label.setText(name + extension);
+        }
     }
 
     @Override
@@ -61,29 +67,34 @@ public class EditorArea extends JTextPane implements KeyListener{
         this.setCharacterAttributes(as, false);
     }
 
-    public void close(){
+    public void close() {
         int option = JOptionPane.showConfirmDialog(this, "Do you want to save file?", "Exit", JOptionPane.YES_NO_OPTION);
         System.out.println(option);
 
-        if (option==0) {
-            String name = JOptionPane.showInputDialog(this, "What's your name, boi (or girl, I am not sexist)");
-            if (name != null && !name.isEmpty()) {
-                fileName = name;
-            }
-            System.out.println(fileName);
-            save();
-            label.setText(fileName+extension);
-//            System.exit(0);
+        if (option == 0) {
+//            save();
+            System.exit(0);
         } else if (option == 1) {
             System.exit(0);
         }
     }
 
-    public void save(){
+    public void save(boolean state) {
+        // state 0 - save as
+        // state 1 - save
+        if (!state) {
+            String name = JOptionPane.showInputDialog(this, "What's your name, boi (or girl, I am not sexist)", fileName);
+            if (name != null && !name.isEmpty()) {
+                setFileName(name);
+            }
+        } else {
+            String name = fileName + extension;
+        }
+
         try {
             File file = new File(PATH + fileName + extension);
             BufferedWriter out = new BufferedWriter(new FileWriter(file));
-            HTMLWriter hw = new HTMLWriter(out, (HTMLDocument)this.getDocument());
+            HTMLWriter hw = new HTMLWriter(out, (HTMLDocument) this.getDocument());
             hw.write();
 
             out.flush();
@@ -93,36 +104,55 @@ public class EditorArea extends JTextPane implements KeyListener{
         }
     }
 
-    public void open(){
+    public void open() {
         File dir = new File(PATH);
         File[] dirListing = dir.listFiles();
-        ArrayList<File> avFiles = new ArrayList<>();
+        ArrayList<String> avFiles = new ArrayList<>();
 //        String[] avFiles = new String[dirListing.length];
 
         if (dirListing != null) {
-            for (File child : dirListing){
+            for (File child : dirListing) {
                 if (child.isFile() && child.getName().endsWith(".html")) {
-                    try {
-                        avFiles.add(child.getCanonicalFile());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    avFiles.add(child.getName());
                     System.out.println(child.getName());
                 }
             }
         }
 
-
-
-        if (avFiles.isEmpty()){
-            System.out.println("dir is empty");
+        if (avFiles.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Directory does not contain any .html files");
         } else {
-            File[] files = avFiles.toArray(new File[avFiles.size()]);
+            String[] files = avFiles.toArray(new String[avFiles.size()]);
             System.out.println("dir is not empty");
-            File result = (File)JOptionPane.showInputDialog(this, "Выберите файл", "Открыть файл для редактирования",
+            String result = (String) JOptionPane.showInputDialog(this, "Выберите файл", "Открыть файл для редактирования",
                     JOptionPane.QUESTION_MESSAGE, null, files, files[0]);
-            System.out.println(result.getName());
+            System.out.println(result);
+
+            try {
+                BufferedReader bf = new BufferedReader(new FileReader(PATH + result));
+                HTMLEditorKit kit = (HTMLEditorKit) this.getEditorKit();
+                HTMLDocument doc = (HTMLDocument) this.getDocument();
+                kit.read(bf, doc, 0);
+                setFileName(result.replace(extension, ""));
+            } catch (IOException | BadLocationException e) {
+                e.printStackTrace();
+            }
+
+
+//            try {
+////                BufferedReader br = new BufferedReader(new FileReader(PATH+result));
+////                String line;
+////                this.setText("");
+////                while((line = br.readLine()) != null){
+////                    System.out.println(line);
+////                    this.add
+////                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
         }
+
+
     }
 
 }
