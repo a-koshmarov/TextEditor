1b70d242a57cc90adf77b87bb859098cca4bd3b8
Alexander Komshanov <a.kosmhanovjr@gmail.com>
2018-10-16 18:38:45 +0300

    working checkboxes

--- src/App/AppView.java
+++ src/App/AppView.java
@@ -4,6 +4,8 @@ import EditorCommands.*;
 import Utility.CustomColor;
 
 import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
@@ -13,16 +15,20 @@ import java.util.HashMap;
 import java.util.Random;
 
 //Invoker
-public class AppView extends JFrame implements ActionListener {
+public class AppView extends JFrame implements ActionListener, CaretListener {
     private HashMap<Object, Command> _commandsByItem;
     private EditorArea editor;
     private int count = 0;
+    private boolean isBold = false;
+    private boolean isItalic = false;
+    private Color curColor = Color.BLACK;
 
     public AppView() {
         super("Text editor");
         _commandsByItem = new HashMap<>();
     }
-    private Color randColor(){
+
+    private Color randColor() {
         Random random = new Random();
         float r = random.nextFloat();
         float g = random.nextFloat();
@@ -30,7 +36,7 @@ public class AppView extends JFrame implements ActionListener {
         return new Color(r, g, b);
     }
 
-    private JMenuBar setupMenu(JLabel label){
+    private JMenuBar setupMenu(JLabel label) {
         JMenuBar mb = new JMenuBar();
         JMenu file, edit, help;
         JMenuItem newFile, openFile, saveFile, saveFileAs, closeAction;
@@ -82,8 +88,8 @@ public class AppView extends JFrame implements ActionListener {
         return mb;
     }
 
-    private JMenu setupStyleMenu(){
-        HashMap<Object, Color>  _colorsByItem = new HashMap<>();
+    private JMenu setupStyleMenu() {
+        HashMap<Object, Color> _colorsByItem = new HashMap<>();
         JMenu mainMenu = new JMenu("Edit");
         JMenu colorMenu = new JMenu("Colors");
         mainMenu.setMnemonic(KeyEvent.VK_E);
@@ -98,17 +104,11 @@ public class AppView extends JFrame implements ActionListener {
                 SimpleAttributeSet as = new SimpleAttributeSet();
                 as.addAttribute(StyleConstants.Foreground, color);
                 editor.setCharacterAttributes(as, false);
+                curColor = color;
             }
         };
 
-        ActionListener checkBoxListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        };
-
-        for (CustomColor col : CustomColor.colors){
+        for (CustomColor col : CustomColor.colors) {
             JMenuItem item = new JMenuItem(col.toString());
             item.addActionListener(colorListener);
             colorMenu.add(item);
@@ -118,26 +118,66 @@ public class AppView extends JFrame implements ActionListener {
         mainMenu.add(colorMenu);
 
         JCheckBoxMenuItem bold = new JCheckBoxMenuItem("Bold");
-        bold.addActionListener(checkBoxListener);
+        bold.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JMenuItem jmi = (JMenuItem) e.getSource();
+                SimpleAttributeSet as = new SimpleAttributeSet();
+                boolean selected = jmi.getModel().isSelected();
+                if (selected) {
+                    as.addAttribute(StyleConstants.Bold, true);
+                    isBold = true;
+                } else {
+                    as.addAttribute(StyleConstants.Bold, false);
+                    isBold = false;
+                }
+                editor.setCharacterAttributes(as, false);
+            }
+        });
+
         mainMenu.add(bold);
 
         JCheckBoxMenuItem italic = new JCheckBoxMenuItem("Italic");
-        italic.addActionListener(checkBoxListener);
+        italic.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JMenuItem jmi = (JMenuItem) e.getSource();
+                SimpleAttributeSet as = new SimpleAttributeSet();
+                boolean selected = jmi.getModel().isSelected();
+                if (selected) {
+                    as.addAttribute(StyleConstants.Italic, true);
+                    isItalic = true;
+                } else {
+                    as.addAttribute(StyleConstants.Italic, false);
+                    isItalic = false;
+                }
+                editor.setCharacterAttributes(as, false);
+            }
+        });
         mainMenu.add(italic);
 
         return mainMenu;
     }
+
     private void setupUI() {
         JPanel panel = new JPanel();
         JLabel fileName = new JLabel();
         editor = new EditorArea(fileName);
         JMenuBar mb = setupMenu(fileName);
 
+//        JTabbedPane tabMenu = new JTabbedPane();
+//        tabMenu.addTab("Tab1", editor);
+//        tabMenu.addTab("Tab2", new JLabel("hallo"));
+//        tabMenu.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+
         editor.setBounds(0, 0, 360, 320);
+        editor.addCaretListener(this);
+        editor.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.add(editor);
         panel.add(mb);
+//        panel.add(tabMenu);
 
         getContentPane().add(panel);
         this.setJMenuBar(mb);
@@ -160,5 +200,14 @@ public class AppView extends JFrame implements ActionListener {
 //        ((JButton) e.getSource()).setBackground(Color.RED);
         Command command = _commandsByItem.get(e.getSource());
         command.execute();
+    }
+
+    @Override
+    public void caretUpdate(CaretEvent e) {
+        SimpleAttributeSet as = new SimpleAttributeSet();
+        as.addAttribute(StyleConstants.Foreground, curColor);
+        as.addAttribute(StyleConstants.Bold, isBold);
+        as.addAttribute(StyleConstants.Italic, isItalic);
+        editor.setCharacterAttributes(as, false);
     }
 }
